8b99a146e7bc14850b27230ab11e389d49c90e4c
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-11-11 18:17:08 -0600

    y

---
+++ portafolio/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// static files
+app.use(express.static( __dirname + '/dist/portafolio'));
+
+// Mandar las peticiones al index.html
+app.get('/*', function(req,res){
+    res.sendFile(path.join( __dirname + '/dist/portafolio/index.html'));
+});
+
+// Puerto default de Heroku
+app.listen(process.env.PORT || 500);
