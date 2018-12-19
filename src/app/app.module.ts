0925ed69e96d4fb5be07e782ed3ddf030c497eba
Michele <michbecerra@hotmail.com>
2018-12-18 20:01:33 -0600

    Correcció en la maquetación de la barra del portaolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppComponent } from './app.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectoComponent } from './components/proyectos/proyectos.component';
+import { BarraComponent} from './components/barra/barra.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     PerfilComponent,
     SkillsComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/barra/barra.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barra',
+  templateUrl: './barra.component.html',
+  styleUrls: ['./barra.component.css']
+})
+export class BarraComponent {
+ public barra_componente =  'Michele Becerra';
+}
